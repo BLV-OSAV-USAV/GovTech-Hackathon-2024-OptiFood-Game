0bd91b008fb550916778ce65e7e0857fb362240f
mwannier30 <maelle.wannier@unifr.ch>
2024-03-14 17:11:00 +0100

    update branch

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+// Add event listeners to the buttons with context binding
+document.getElementById('strtChallenge').addEventListener('click', function() {
+    window.location.href = 'pages/quiz.html';
+});
