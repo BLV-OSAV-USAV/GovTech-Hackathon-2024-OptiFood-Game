e0418b3b233f415443431a245a76b17df85538c9
mwannier30 <maelle.wannier@unifr.ch>
2024-03-14 18:30:36 +0100

    update ranking

---
+++ js/usrCall.js
@@ -0,0 +1,48 @@
+function CallUser() {
+    var request = new XMLHttpRequest(); // Create a request variable and assign a new XMLHttpRequest object to it.
+    // Other requests
+    // https://optifood.directus.app/items/QuizzQuestion?quiz=3
+    // request.open('GET', 'https://optifood.directus.app/items/Challenges'); // 
+    request.open('GET', 'https://optifood.directus.app/items/Score?fields=*.*.*.*'); 
+    request.send();
+/*     request.open('GET', 'https://optifood.directus.app/items/Friend'); 
+    request.send(); */
+    
+
+
+    let usr_arr = []; // Initialize an empty array outside the onload function
+
+    request.onload = async function () {
+        var data = JSON.parse(this.response);
+        for (var j = 0; j < data.data.length; j++){
+            let usr = {
+                id: data.data[j].related_user.id, 
+                first_name: data.data[j].related_user.first_name,
+                last_name: data.data[j].related_user.last_name,
+                reached_score: data.data[j].reached_score
+            };
+            usr_arr.push(usr);
+        }
+    
+        // Sort users array by reached_score in descending order
+        usr_arr.sort((a, b) => b.reached_score - a.reached_score);
+    
+        let html = '<table>';
+    
+        usr_arr.forEach((user, i) => { // Use forEach to iterate over usr_arr
+            html += `
+                <tr>
+                    <td class="number">${i + 1}</td>
+                    <td class="name">${user.first_name} ${user.last_name}</td>
+                    <td class="points">${i === 0 ? user.reached_score + '<img class="gold-medal" src="https://github.com/malunaridev/Challenges-iCodeThis/blob/master/4-leaderboard/assets/gold-medal.png?raw=true" alt="gold medal"/>' : user.reached_score}</td>
+                </tr>
+            `;
+        });        
+    
+        html += '</table>';
+        
+        // Replace the inner HTML of userList with the generated HTML
+        document.getElementById('ranking').innerHTML = html;
+    };
+}
+CallUser()
